cc350047cebe9502633a90643bdce73dc759e0d1
chimbuchieke <113604794+chimbuchieke@users.noreply.github.com>
2023-04-26 17:52:28 +0100

    task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,16 @@
-#client configuration file
+# Set up client configuration file
 include stdlib
-first_line {'set identity file':
 
+file_line { 'Turn off password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Delare identity file location':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true
+}
